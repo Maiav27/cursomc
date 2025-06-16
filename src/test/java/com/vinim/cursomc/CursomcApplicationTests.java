ff73f8342dd9f40de88378d0b148acf879e15a5f
Vinícius <viniciusmaiasg@gmail.com>
2025-06-16 08:55:24 -0300

    primeiro commit

---
+++ src/test/java/com/vinim/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinim.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
