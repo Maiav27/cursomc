3a37d9ea461f6d9fed20f7f2c733fe592478b983
Vinícius Maia <viniciusmaiasg@gmail.com>
2025-06-16 10:48:41 -0300

    testando a primeria classe dominio -Categoria

--- src/main/java/com/vinim/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vinim/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.vinim.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vinim.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,15 @@ public class CategoriaResource {
 	
 	
 	@GetMapping
-	public String Listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
