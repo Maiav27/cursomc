88fd64dceb55ebaaa615241e9f56e4f7979abc2f
Vinícius Maia <viniciusmaiasg@gmail.com>
2025-07-21 11:42:00 -0300

    PUT, DELETE E GET cliente

---
+++ src/main/java/com/vinim/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,63 @@
+package com.vinim.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.vinim.cursomc.domain.Cliente;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min=5, max=120 ,message="O tamanho deve ter entre 5 e 120 caracteres")
+	private String nome;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "email inválido")
+	private String email;
+
+	public ClienteDTO() {
+
+	}
+
+	public ClienteDTO(Cliente cliente) {
+		super();
+		this.id = cliente.getId();
+		
+		this.nome = cliente.getNome();
+		this.email = cliente.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/vinim/cursomc/service/ClienteService.java
+++ src/main/java/com/vinim/cursomc/service/ClienteService.java
@@ -4,10 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.vinim.cursomc.domain.Categoria;
 import com.vinim.cursomc.domain.Cliente;
+import com.vinim.cursomc.domain.Cliente;
+import com.vinim.cursomc.dto.ClienteDTO;
 import com.vinim.cursomc.repositories.ClienteRepository;
+import com.vinim.cursomc.service.exceptions.DataIntegrityException;
 import com.vinim.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,14 +25,59 @@ public class ClienteService {
 	private ClienteRepository repo;
 	
 	
-	public Cliente buscar(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
-	    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + "Tipo:" + Cliente.class.getName()));
-	}
+	
 	
 	
 	public List<Cliente> listarCliente(){
 		 return repo.findAll();
+	}
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(	"Objeto não encontrado! Id:" + 	id + ", Tipo: " + Cliente.class.getName())); //sozinho , obj é Optional<Category> , mas obj.orElse ele retorna um objeto do tipo Cliente;
+
+	}
+	
+	public List<Cliente> listar() {
+	
+		return repo.findAll();
+		}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy );
+		return repo.findAll(pageRequest);
+	}
+    
+	public Cliente insert(Cliente obj) {
+		
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.buscar(obj.getId());
+		this.updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		this.buscar(id);
+		try {
+			
+			this.repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+	}
+	
+	
+	public Cliente dtoToCliente(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
   
 }
